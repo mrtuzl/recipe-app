f04e4dc8ab27dcdffb669642dc70fd04aa3920e3
mrtuzl <mertuzel666@gmail.com>
2023-10-15 23:58:14 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                }
+            },
+            {
+                    test: /\.scss$/,  
+                    use: ['style-loader', 'css-loader', 'sass-loader'],  
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'images/',
+                    },
+                  },
+                ]
+            }
+        ]
+    }
+
+}
